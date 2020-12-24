26b863c7896e63be9dc5a9585a0fabcf98c4e2f4
GmwEnterprise <909498022@qq.com>
2020-12-24 17:51:07 +0800

    gmw

--- hello-clion-projects/hello-world/main.c
+++ hello-clion-projects/hello-world/main.c
@@ -8,9 +8,11 @@ void func1(void);
 void platinum(void);
 // 基本类型测试
 void basicTypeTest();
+// 打印变量的各种方法
+void printVariables();
 
 int main(void) {
-    basicTypeTest();
+    printVariables();
     return 0;
 }
 
@@ -42,7 +44,33 @@ void platinum(void) {
 }
 
 void basicTypeTest() {
-    unsigned char ch = 256;
+    unsigned char ch = 255;
     printf("ch = %d", ch);
     double d = 3.5E-8;
+}
+
+void printVariables() {
+    int x = 100;
+    // 十进制 八进制 十六进制
+    printf("dec = %d, oct = %o, hex = %x\n", x, x, x);
+    // 添加一个 # 号可以打印出八进制与十六进制的前缀
+    printf("dec = %d, oct = %#o, hex = %#x\n", x, x, x);
+
+    int i = 0x7FFFFFFF;
+    unsigned int j = 0xFFFFFFFF;
+    // %u 打印 unsigned int 类型，如果传递给 %u 的参数是有符号数，则会转换为无符号数
+    // 数值溢出后会重新从起点开始
+    printf("%d %d %d\n", i, i + 1, i + 2);
+    printf("%u %u %u\n", j, j + 1, j + 2);
+
+    unsigned int un = 3000000000;
+    short end = 200;
+    long big = 65537;
+    long long verybig = 12345678908642;
+
+    // 打印符规则
+    printf("un = %u and not %d\n", un, un);
+    printf("end = %hd and not %d\n", end, end);
+    printf("big = %ld and not %hd\n", big, big);
+    printf("verybig = %lld and not %ld\n", verybig, verybig);
 }
