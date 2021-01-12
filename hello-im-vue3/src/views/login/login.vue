3e04d1d1fc997ca0a7da86b8bc5cd74adb5a53f4
GmwEnterprise <909498022@qq.com>
2021-01-12 18:03:18 +0800

    gmw

--- hello-im-vue3/src/views/login/login.vue
+++ hello-im-vue3/src/views/login/login.vue
@@ -1,3 +1,12 @@
 <template>
   <div id="login-view">Log in</div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Login",
+  props: {},
+});
+</script>
