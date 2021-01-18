9d961503da3ae0b0353dc58feed9abe2cb77f117
GmwEnterprise <909498022@qq.com>
2021-01-18 17:53:15 +0800

    gmw

---
+++ hello-vue3-json-schema-form/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+interface Config {
+  name: string;
+}
+
+export default defineComponent({
+  name: 'App',
+  props: {
+    age: {
+      type: Number as PropType<number>,
+    },
+    config: Object as PropType<Config>,
+  },
+  components: {
+    HelloWorld,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
