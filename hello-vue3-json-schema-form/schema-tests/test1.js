51c9c5edb552f2112a56b3c0c057acd294b54f00
GmwEnterprise <909498022@qq.com>
2021-01-22 17:25:26 +0800

    gmw

--- hello-vue3-json-schema-form/schema-tests/test1.js
+++ hello-vue3-json-schema-form/schema-tests/test1.js
@@ -1,14 +1,11 @@
-const Ajv = require('ajv').default;
+const Ajv = require('ajv').default
+// 版本7开始，需要单独引入这个东西
+const addFormats = require('ajv-formats')
 
 const schema = {
   type: 'object',
   properties: {
     name: {
-      type: 'string',
-      // format: 'myname',
-      // minLength: 10,
-    },
-    bindEmail: {
       type: 'string',
       format: 'email',
     },
@@ -26,21 +23,18 @@ const schema = {
     },
   },
   required: ['name', 'age'],
-};
+}
 
-const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
-
+const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
+addFormats(ajv)
 // ajv.addFormat('myname', (value) => {
 //   console.log('custom-format');
 //   return value === 'Mrag';
 // });
 
-const validate = ajv.compile(schema);
+const validate = ajv.compile(schema)
 const valid = validate({
   name: 'Mrag',
-  bindEmail: '909498022@qq.com',
   age: 24,
-  // pets: ['mimi', 'mama'],
-  // isWorker: true,
-});
-if (!valid) console.log(validate.errors);
+})
+if (!valid) console.log(validate.errors)
