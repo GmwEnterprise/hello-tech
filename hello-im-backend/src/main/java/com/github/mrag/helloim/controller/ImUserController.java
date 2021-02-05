e204e3efe9e01de81f650b171001db16a9f8c3ea
GmwEnterprise <909498022@qq.com>
2021-02-05 17:48:45 +0800

    gmw

--- hello-im-backend/src/main/java/com/github/mrag/helloim/controller/ImUserController.java
+++ hello-im-backend/src/main/java/com/github/mrag/helloim/controller/ImUserController.java
@@ -1,15 +1,21 @@
 package com.github.mrag.helloim.controller;
 
+import com.github.mrag.helloim.dao.ImUserMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/demo")
 public class HelloImController {
 
+    @Resource
+    ImUserMapper imUserMapper;
+
     @GetMapping
     public String sayHello() {
-        return "Hello !";
+        return "Hello !" + imUserMapper.selectAll();
     }
 }
