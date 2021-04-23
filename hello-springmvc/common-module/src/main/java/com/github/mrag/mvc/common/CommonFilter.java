3b9e779c941d9effe14fcd6637e3a20dd95c38ed
ganmingwei <909498022>
2021-04-23 17:32:24 +0800

    gmw

--- hello-springmvc/common-module/src/main/java/com/github/mrag/mvc/common/CommonFilter.java
+++ hello-springmvc/common-module/src/main/java/com/github/mrag/mvc/common/CommonFilter.java
@@ -1,5 +1,8 @@
 package com.github.mrag.mvc.common;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.*;
 import java.io.IOException;
 
@@ -7,11 +10,17 @@ import java.io.IOException;
  * @author Gmw
  */
 public class CommonFilter implements Filter {
+    private static final Logger log = LoggerFactory.getLogger(CommonFilter.class);
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         System.out.println("过滤器 => before");
+        displayRequestMetadata(request);
         chain.doFilter(request, response);
         System.out.println("过滤器 => after");
+    }
+
+    private void displayRequestMetadata(ServletRequest request) {
+        log.info("remoteHost={}, remotePort={}", request.getRemoteHost(), request.getRemotePort());
     }
 }
--- hello-springmvc/demo-api/src/main/java/com/github/mrag/mvc/DemoApp.java
+++ hello-springmvc/demo-api/src/main/java/com/github/mrag/mvc/DemoApp.java
@@ -1,9 +1,22 @@
 package com.github.mrag.mvc;
 
+import com.github.mrag.mvc.common.CommonFilter;
+import com.github.mrag.mvc.common.CommonInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * @author Gmw
@@ -18,6 +31,35 @@ public class DemoApp {
 
     @GetMapping
     String sayHello() {
-        return "Hello SpringMVC !";
+        return "Hello SpringMVC !\n";
+    }
+
+    @PostMapping(value = "/", produces = {
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE
+    })
+    Map<String, Object> postHello() {
+        LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+        map.put("timestamp", System.currentTimeMillis());
+        map.put("message", "OK");
+        return map;
+    }
+
+    @Configuration
+    @EnableWebMvc
+    static class WebConf implements WebMvcConfigurer {
+        @Override
+        public void addInterceptors(InterceptorRegistry registry) {
+            registry.addInterceptor(new CommonInterceptor());
+        }
+
+        @Bean
+        FilterRegistrationBean<CommonFilter> createCommonFilter() {
+            FilterRegistrationBean<CommonFilter> filter = new FilterRegistrationBean<>();
+            filter.setFilter(new CommonFilter());
+            filter.setOrder(1);
+            filter.addUrlPatterns("/*");
+            return filter;
+        }
     }
 }
