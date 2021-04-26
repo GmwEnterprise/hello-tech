c4b84f0d6e090c76792a3b11fa6f600f532eac91
gmw <909498022@qq.com>
2021-04-27 00:03:25 +0800

    gmw

---
+++ hello-c-projects/helloworld1/main.c
@@ -0,0 +1,11 @@
+//
+// Created by Gmw on 2021/4/26.
+//
+
+#include <stdio.h>
+
+int main() {
+    int num = 5;
+    printf("Hello world !, num = %d\n", num);
+    return 0;
+}
--- hello-c-projects/src/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-  int value = 5;
-  printf("Hello C Programming! and value = %d.", value);
-  getchar();
-  return 0;
-}
