bd138d4858c89d243541ccde2bbdfbde22b490a0
gmw <909498022@qq.com>
2021-08-06 01:28:31 +0800

    gmw

---
+++ hello-distributed-im/dim-infrastructure/dim-infrastructure-transport/src/main/java/com/github/mrag/dim/transport/NettyTest.java
@@ -0,0 +1,10 @@
+package com.github.mrag.dim.transport;
+
+import io.netty.bootstrap.Bootstrap;
+
+public class NettyTest {
+
+    public static void main(String[] args) {
+        Bootstrap bootstrap = new Bootstrap();
+    }
+}
